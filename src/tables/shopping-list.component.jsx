40682e5b908b110a757c8df1302f279eb3947426
Emmanuel Okoh <emmanuel.okoh@ageas.co.uk>
2020-02-12 14:56:43 +0000

    added shoppingList, AddItem and EditList components as well as modified package.json

---
+++ src/tables/shopping-list.component.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const ShoppingList = props => (
+  <table>
+    <thead>
+      <tr></tr>
+    </thead>
+    <tbody>
+      {props.items.length > 0 ? (
+        props.items.map(item => (
+          <tr key={item.id}>
+            <td>{item.name}</td>
+            <td>
+              <button
+                onClick={() => {
+                  props.editRow(item);
+                }}
+                className="button muted-button"
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => props.deleteItem(item.id)}
+                className="button muted-button"
+              >
+                Remove
+              </button>
+
+              <input type="checkbox" />
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={3}>No item</td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+);
+
+export default ShoppingList;
